08d97cb0e63413d1dea07ce08ea37e3fc289ff3e
akaWild <nemyx84@gmail.com>
2024-07-09 21:09:04 +0300

    Added private fields to Chess class

--- ChessDotNet/Chess.cs
+++ ChessDotNet/Chess.cs
@@ -4,7 +4,28 @@ namespace ChessDotNet
 {
     public class Chess
     {
-        private readonly Dictionary<string, string> _headers = new Dictionary<string, string>();
+        private ChessColor _turn = ChessColor.White;
+        private int _epSquare = -1;
+        private int _halfMoves = 0;
+        private int _moveNumber = 0;
+
+        private ChessPiece[] _board = new ChessPiece[128];
+
+        private readonly List<ChessHistory> _history = new();
+
+        private Dictionary<ChessColor, int> _kings = new()
+        {
+            { ChessColor.White, InternalData.Empty},
+            { ChessColor.Black, InternalData.Empty},
+        };
+        private Dictionary<ChessColor, int> _castling = new()
+        {
+            { ChessColor.White, 0},
+            { ChessColor.Black, 0},
+        };
+        private readonly Dictionary<string, string> _headers = new();
+        private readonly Dictionary<string, string> _comments = new();
+        private readonly Dictionary<string, int> _positionCount = new();
 
         public void SetHeader(PngHeader pngHeader) => _headers[pngHeader.Key] = pngHeader.Value;
 
