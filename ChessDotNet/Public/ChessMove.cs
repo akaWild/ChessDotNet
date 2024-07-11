60ff1889e07aea8ab70757f569ca07f645f6e559
akaWild <nemyx84@gmail.com>
2024-07-11 17:57:35 +0300

    Made some props in ChessMove settable

--- ChessDotNet/Public/ChessMove.cs
+++ ChessDotNet/Public/ChessMove.cs
@@ -1,4 +1,10 @@
 ﻿namespace ChessDotNet.Public
 {
-    public record ChessMove(ChessColor Color, ChessSquare From, ChessSquare To, ChessPieceType Piece, ChessPieceType? Captured, ChessPieceType? Promotion, string Flags, string San, string Lan, string Before, string After);
+    public record ChessMove(ChessColor Color, ChessSquare From, ChessSquare To, ChessPieceType Piece, ChessPieceType? Captured, ChessPieceType? Promotion, string Flags, string San, string Lan, string Before, string After)
+    {
+        public string After { get; set; } = After;
+        public ChessPieceType? Captured { get; set; } = Captured;
+        public ChessPieceType? Promotion { get; set; } = Promotion;
+        public string Lan { get; set; } = Lan;
+    }
 }
