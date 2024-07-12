cb56fb9a1b72da7248428be63aebbd14421e194a
akaWild <nemyx84@gmail.com>
2024-07-12 17:54:32 +0300

    Added tests for checkmate and draw

--- ChessDotNet.Test/ChessTests.cs
+++ ChessDotNet.Test/ChessTests.cs
@@ -474,5 +474,32 @@ namespace ChessDotNet.Tests
         }
 
         #endregion
+
+        #region IsCheckmate
+
+        [Theory]
+        [InlineData("8/5r2/4K1q1/4p3/3k4/8/8/8 w - - 0 7")]
+        [InlineData("4r2r/p6p/1pnN2p1/kQp5/3pPq2/3P4/PPP3PP/R5K1 b - - 0 2")]
+        [InlineData("r3k2r/ppp2p1p/2n1p1p1/8/2B2P1q/2NPb1n1/PP4PP/R2Q3K w kq - 0 8")]
+        [InlineData("8/6R1/pp1r3p/6p1/P3R1Pk/1P4P1/7K/8 b - - 0 4")]
+        public void IsCheckmate_ReturnsTrue(string fen)
+        {
+            var chess = new Chess(fen);
+
+            Assert.True(chess.IsCheckmate());
+            Assert.False(chess.IsDraw());
+        }
+
+        [Theory]
+        [InlineData(PublicData.DefaultChessPosition)]
+        [InlineData("1R6/8/8/8/8/8/7R/k6K b - - 0 1")]
+        public void IsCheckmate_ReturnsFalse(string fen)
+        {
+            var chess = new Chess(fen);
+
+            Assert.False(chess.IsCheckmate());
+        }
+
+        #endregion
     }
 }
