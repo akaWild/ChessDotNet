7d3dcf95cf6d6e53e70a3fd1704c7a7b4816c941
akaWild <nemyx84@gmail.com>
2024-07-13 21:02:13 +0300

    Added more position count tests

--- ChessDotNet.Test/PositionCountTests.cs
+++ ChessDotNet.Test/PositionCountTests.cs
@@ -108,5 +108,21 @@ namespace ChessDotNet.Tests
             Assert.Equal(1, (int)(_fixture.GetPositionCountMethod?.Invoke(chess, new object[] { newFen })));
             Assert.Equal(1, (int)(((Dictionary<string, int>)_fixture.PositionCountDictionary?.GetValue(chess)).Keys.Count));
         }
+
+        [Fact]
+        public void PositionCount_ResetsWhenLoadingPgn()
+        {
+            var chess = new Chess();
+
+            chess.Move("e4");
+            chess.LoadPgn("1. d4 d5");
+
+            Assert.Equal(1, (int)(_fixture.GetPositionCountMethod?.Invoke(chess, new object[] { PublicData.DefaultChessPosition })));
+            Assert.Equal(0, (int)(_fixture.GetPositionCountMethod?.Invoke(chess, new object[] { PositionCountFixture.E4Fen })));
+
+            Assert.Equal(1, (int)(_fixture.GetPositionCountMethod?.Invoke(chess, new object[] { "rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq - 0 1" })));
+            Assert.Equal(1, (int)(_fixture.GetPositionCountMethod?.Invoke(chess, new object[] { "rnbqkbnr/ppp1pppp/8/3p4/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 0 2" })));
+            Assert.Equal(3, (int)(((Dictionary<string, int>)_fixture.PositionCountDictionary?.GetValue(chess)).Keys.Count));
+        }
     }
 }
